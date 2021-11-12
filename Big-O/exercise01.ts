15f8a44062d1d9ba51c2a24d002dc8672296734b
Chantelsky <laver.chantelsky@gmail.com>
2021-11-13 09:15:31 +1000

    exercise 1 of Big-O

---
+++ Big-O/exercise01.ts
@@ -0,0 +1,16 @@
+function anotherFunction() {
+    //unknown function
+}
+
+function funChallenge(input: Array<any>): number {
+    let a = 10;
+    a = 50 + 3;
+
+    for (let i = 0; i < input.length; i++) {
+        anotherFunction();
+        let stranger = true;
+        a++;
+    }
+
+    return a;
+}
