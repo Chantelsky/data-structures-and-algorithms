01914eba92034c14fe3489470e2ac92faaebfda5
Chantelsky <laver.chantelsky@gmail.com>
2021-11-19 20:19:33 +1000

    first recurring character

--- Data-Structures/Hash-Tables/HashTable.js
+++ Data-Structures/Hash-Tables/HashTable.js
@@ -53,3 +53,42 @@ myHashTable.get('grapes')
 myHashTable.set('apples', 9)
 myHashTable.get('apples')
 myHashTable.keys()
+
+//---------------------------------------------------------------------
+// ----------          Exercise first recurring character    ----------
+//---------------------------------------------------------------------
+
+//Given an array = [2,5,1,2,3,5,1,2,4]
+//it should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]
+//it should return 1
+
+//Given an array = [2,3,4,5]
+//it should return undefined
+
+// function firstRecurringCharacter(input) {
+//   for (let i = 0; i < input.length; i++) {
+//     for (let j = i + 1; j < input.length; j++) {
+//       if (input[i] === input[j]) {
+//         return input[i]
+//       }
+//     }
+//   }
+//   return undefined
+// } O(n^2)
+
+function firstRecurringCharacter(input) {
+  let hashMap = {}
+  for (let i = 0; i < input.length; i++) {
+    if (hashMap[input[i]]) {
+      return input[i]
+    } else {
+      hashMap[input[i]] = true
+    }
+  }
+  return undefined
+} //O(n)
+
+firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
+
